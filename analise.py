bc1cef2833043fc3709749ac78cc6e9f5c03c12d
PauloEngineer <paulo-engineer@hotmail.com>
2025-10-22 15:02:42 -0300

    feauter: Update

--- analise.py
+++ analise.py
@@ -30,3 +30,30 @@ Ra226_menor_8 = ((df['Resultado_ra226'] > 1) & (df['Resultado_ra226'] <= 8)).sum
 # %%
 Ra226_menor_8
 # %%
+# Contando a frequência de cada categoria
+contagem = df['Tipo de Resíduo'].value_counts()
+contagem
+
+# %%
+# Coluna de RA226
+coluna_ra226 = df['Resultado_ra226']
+coluna_ra226
+
+# %%
+# Criando faixas RA226
+coluna_ra226 = pd.cut(df["Resultado_ra226"], bins=10)
+contagem_ra226 = coluna_ra226.value_counts().sort_index()
+contagem_ra226
+
+# %%
+# Coluna de (µSv/h)
+coluna_dose_µSvh = df['Taxa de Dose Máxima (µSv/h)']
+coluna_dose_μSvh
+
+# %%
+# Criando faixas (µSv/h)
+coluna_dose_µSvh = pd.cut(df["Taxa de Dose Máxima (µSv/h)"], bins=8)
+contagem_dose = coluna_dose_µSvh.value_counts().sort_index()
+
+contagem_dose
+# %%
